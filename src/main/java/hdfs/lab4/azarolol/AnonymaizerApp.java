eb53f7e03fddf4e5b8040163c2d35e56f5a14010
Azarolol <gimazovazamat@mail.ru>
2021-12-24 00:29:49 +0300

    Scripted auto-commit on change (2021-12-24 00:29:49) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/AnonymaizerApp.java
+++ src/main/java/hdfs/lab4/azarolol/AnonymaizerApp.java
@@ -1,5 +1,10 @@
 package hdfs.lab4.azarolol;
 
+import akka.actor.ActorSystem;
+
 public class AnonymaizerApp {
-    
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
+        ActorRef 
+    }
 }
