0c311f62bf95da6d72627cef1bd0f3f52a730564
Azarolol <gimazovazamat@mail.ru>
2021-12-24 00:52:35 +0300

    Scripted auto-commit on change (2021-12-24 00:52:35) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/ConfigStorageActor.java
+++ src/main/java/hdfs/lab4/azarolol/ConfigStorageActor.java
@@ -3,4 +3,9 @@ package hdfs.lab4.azarolol;
 import akka.actor.AbstractActor;
 
 public class ConfigStorageActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
