2fb51dc4951abceaca737996c9a20fc992c9d94a
Azarolol <gimazovazamat@mail.ru>
2021-12-24 00:39:18 +0300

    Scripted auto-commit on change (2021-12-24 00:39:18) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/AnonymaizerApp.java
+++ src/main/java/hdfs/lab4/azarolol/AnonymaizerApp.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class AnonymaizerApp {
     private static final String ACTOR_SYSTEM_NAME = "AnonymizerActors";
-    private static final int INDEX_OF_ADRESS = 0;
+    private static final int INDEX_OF_SERVER = 0;
     private static final int ZOOKEEPER_TIMEOUT = 3000;
     public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
@@ -19,7 +19,7 @@ public class AnonymaizerApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        ZooKeeper zoo = new ZooKeeper(args[INDEX_OF_ADRESS], ZOOKEEPER_TIMEOUT, null);
+        ZooKeeper zoo = new ZooKeeper(args[INDEX_OF_SERVER], ZOOKEEPER_TIMEOUT, null);
 
 
     }
