d921362cea7eab4e4436716060f5eec20a1cbbf8
Azarolol <gimazovazamat@mail.ru>
2021-12-24 01:57:56 +0300

    Scripted auto-commit on change (2021-12-24 01:57:56) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/AnonymaizerApp.java
+++ src/main/java/hdfs/lab4/azarolol/AnonymaizerApp.java
@@ -1,12 +1,15 @@
 package hdfs.lab4.azarolol;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
@@ -19,6 +22,7 @@ public class AnonymaizerApp {
     private static final int INDEX_OF_SERVER = 0;
     private static final int ZOOKEEPER_TIMEOUT = 3000;
     private static final String SERVERS_PATH = "/servers";
+    private static final String LOCALHOST = "localhost";
     public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
         ActorRef storage = system.actorOf(Props.create(ConfigStorageActor.class));
@@ -29,12 +33,13 @@ public class AnonymaizerApp {
         ZooKeeper zoo = new ZooKeeper(port, ZOOKEEPER_TIMEOUT, null);
         ZookeeperWatcher watcher = new ZookeeperWatcher(storage, zoo);
 
-        final String url = "localhost:" + port;
+        final String url = LOCALHOST + ":" + port;
         zoo.create(SERVERS_PATH,
                 url.getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE,
                 CreateMode.EPHEMERAL_SEQUENTIAL);
 
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = new HTTPServer(http, storage).createRoute().flow(system, materializer);
+        http.bindAndHandle(routeFlow, ConnectHttp.toHost(LOCALHOST, Integer.parseInt(port)), materializer);
     }
 }
