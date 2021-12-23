2355641035e73075de33f6b3795438cbb97a5865
Azarolol <gimazovazamat@mail.ru>
2021-12-24 00:55:10 +0300

    Scripted auto-commit on change (2021-12-24 00:55:10) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/ConfigStorageActor.java
+++ src/main/java/hdfs/lab4/azarolol/ConfigStorageActor.java
@@ -2,11 +2,16 @@ package hdfs.lab4.azarolol;
 
 import akka.actor.AbstractActor;
 
-public class ConfigStorageActor extends AbstractActor {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ConfigStorageActor extends AbstractActor {
+    private List<String> servers = new ArrayList<>();
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match(ServersListMessage.class, )
+                .match(ServersListMessage.class,
+                        message -> )
+                .match()
     }
 }
