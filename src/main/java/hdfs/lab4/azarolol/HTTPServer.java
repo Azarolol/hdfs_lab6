04a3adf8af9bad91a56ac797f2b7176c1d3c6d98
Azarolol <gimazovazamat@mail.ru>
2021-12-24 01:40:33 +0300

    Scripted auto-commit on change (2021-12-24 01:40:33) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/HTTPServer.java
+++ src/main/java/hdfs/lab4/azarolol/HTTPServer.java
@@ -3,6 +3,8 @@ package hdfs.lab4.azarolol;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.server.Route;
 
+import static akka.http.javadsl.server.Directives.route;
+
 public class HTTPServer {
     private final Http http;
 
@@ -11,6 +13,8 @@ public class HTTPServer {
     }
 
     public Route createRoute() {
-        
+        return route(
+
+        )
     }
 }
