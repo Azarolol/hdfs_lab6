95906ff9835af24df1b41205f09f52807e9f5780
Azarolol <gimazovazamat@mail.ru>
2021-12-24 01:14:33 +0300

    Scripted auto-commit on change (2021-12-24 01:14:33) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/ZookeeperWatcher.java
+++ src/main/java/hdfs/lab4/azarolol/ZookeeperWatcher.java
@@ -1,9 +1,19 @@
 package hdfs.lab4.azarolol;
 
+import akka.actor.ActorRef;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 public class ZookeeperWatcher implements Watcher {
+    private final ActorRef storage;
+    private final ZooKeeper zoo;
+
+    public ZookeeperWatcher(ActorRef storage, ZooKeeper zoo) {
+        this.storage = storage;
+        this.zoo = zoo;
+    }
+
     @Override
     public void process(WatchedEvent watchedEvent) {
 
