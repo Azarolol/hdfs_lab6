6b493cb16fa71d422693417b7cb2a85a04b429ff
Azarolol <gimazovazamat@mail.ru>
2021-12-24 01:34:37 +0300

    Scripted auto-commit on change (2021-12-24 01:34:37) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/AnonymaizerApp.java
+++ src/main/java/hdfs/lab4/azarolol/AnonymaizerApp.java
@@ -4,19 +4,21 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
 import akka.stream.ActorMaterializer;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 public class AnonymaizerApp {
     private static final String ACTOR_SYSTEM_NAME = "AnonymizerActors";
     private static final int INDEX_OF_SERVER = 0;
     private static final int ZOOKEEPER_TIMEOUT = 3000;
-    public static void main(String[] args) throws IOException {
+    private static final String SERVERS_PATH = "/servers";
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
         ActorRef storage = system.actorOf(Props.create(ConfigStorageActor.class));
         final Http http = Http.get(system);
@@ -27,9 +29,11 @@ public class AnonymaizerApp {
         ZookeeperWatcher watcher = new ZookeeperWatcher(storage, zoo);
 
         final String url = "localhost:" + port;
-        zoo.create(
+        zoo.create(SERVERS_PATH,
                 url.getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE,
                 CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        final Flow<HttpRequest, >
     }
 }
